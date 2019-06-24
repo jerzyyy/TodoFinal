b654d33a99b7aa7f2c99e9c3aa264dc809599452
Jerzy Yengo <jerzy.yengo@gmail.com>
2019-06-24 23:52:46 +0200

    ok

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { TodoListComponent } from './todo/todo-list/todo-list.component';
+import { TodoItemComponent } from './todo/todo-item/todo-item.component';
+import { TodoContainerComponent } from './todo/todo-container/todo-container.component';
+import { TodoFormComponent } from './todo/todo-form/todo-form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TodoListComponent,
+    TodoItemComponent,
+    TodoContainerComponent,
+    TodoFormComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/todo/todo-container/todo-container.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { Todo } from '../model/todo';
+
+@Component({
+  selector: 'app-todo-container',
+  templateUrl: './todo-container.component.html',
+  styleUrls: ['./todo-container.component.css']
+})
+export class TodoContainerComponent implements OnInit {
+
+  todoList: Array<Todo> = [];
+
+  addtask(title: string) {
+    this.todoList = [...this.todoList, new Todo( title, false)];
+  }
+
+  removetasks() {
+    this.todoList = [];
+}
+
+  constructor() { }
+
+  ngOnInit() {
+    this.todoList = [
+      new Todo('take the ball', false),
+      new Todo('dribbling', false),
+      new Todo('kidding', false),
+      new Todo('scoring', false)];
+  }
+
+}
